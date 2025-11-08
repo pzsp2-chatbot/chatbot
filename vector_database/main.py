9f4895093205a6cf338d2efdae26d0f3832f766f
mcieslik <01187214@pw.edu.pl>
2025-11-08 20:07:36 +0100

    Created create_collection endpoint template

--- vector_database/main.py
+++ vector_database/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from vector_database.models import CreateCollectionRequest
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/create_collection")
+def create_collection(request: CreateCollectionRequest):
+    pass
