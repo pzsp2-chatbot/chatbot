ef8c91bed7d53f9565bf5e4adc1f54f6f5433c55
mcieslik <01187214@pw.edu.pl>
2025-11-09 11:39:47 +0100

    Fixed endpoint name and added add_item function

--- vector_database/main.py
+++ vector_database/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 
-from vector_database.models import CreateCollectionRequest
+from vector_database.models import CreateCollectionRequest, AddItemRequest
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/create_collection")
+
+@app.post("/collections")
 def create_collection(request: CreateCollectionRequest):
+    pass
+
+
+@app.post("/collections/{collection_name}/items")
+def add_item(collection_name: str, request: AddItemRequest):
     pass
