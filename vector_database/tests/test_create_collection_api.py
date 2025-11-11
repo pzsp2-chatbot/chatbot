e23cad9fa68784b3540245e85d2c5132b94f7650
mcieslik <01187214@pw.edu.pl>
2025-11-11 15:43:45 +0100

    Added collection length check asserts

--- vector_database/tests/test_create_collection_api.py
+++ vector_database/tests/test_create_collection_api.py
@@ -23,51 +23,81 @@ def cleanup_collection():
 
 def test_create_collection_success(cleanup_collection):
     collection_name = cleanup_collection
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": collection_name, "vector_size": 1024})
+    collections_after = len(qdrant_client.get_collections().collections)
 
     assert response.status_code == 200
     data = response.json()
     assert data["status"] == "ok"
-    assert data["message"] == f"Collection {collection_name} created successfully."
+    assert data["message"] == f"Collection '{collection_name}' created successfully."
+    assert collections_before == collections_after - 1
 
     collection_info = qdrant_client.get_collection(collection_name=collection_name)
-    assert collection_info.config.vectors["size"] == 1024
+    assert collection_info.config.params.vectors.size == 1024
 
 def test_create_collection_failed_collection_exists(cleanup_collection):
     collection_name = cleanup_collection
+    collections_before = len(qdrant_client.get_collections().collections)
     client.post("/collections", json={"name": collection_name, "vector_size": 1024})
     response = client.post("/collections", json={"name": collection_name, "vector_size": 1024})
+    collections_after = len(qdrant_client.get_collections().collections)
 
     assert response.status_code == 400
     data = response.json()
-    assert data["status"] == "bad request"
-    assert data["message"] == f"Collection with name {collection_name} already exists."
+    assert data["detail"]["status"] == "bad request"
+    assert data["detail"]["message"] == f"Collection with name '{collection_name}' already exists."
+    assert collections_before == collections_after - 1
 
 def test_create_collection_failed_invalid_name_type_pydantic():
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": 123, "vector_size": 1024})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
 
 def test_create_collection_failed_too_short_name_pydantic():
     name = ""
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": name, "vector_size": 1024})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
 
 def test_create_collection_failed_too_long_name_pydantic():
     name = 65 * "a"
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": name, "vector_size": 1024})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
 
 def test_create_collection_failed_invalid_vector_size_type_pydantic(cleanup_collection):
     collection_name = cleanup_collection
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": collection_name, "vector_size": "abc"})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
 
 def test_create_collection_failed_too_short_vector_pydantic(cleanup_collection):
     collection_name = cleanup_collection
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": collection_name, "vector_size": 1023})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
 
 def test_create_collection_failed_too_long_vector_pydantic(cleanup_collection):
     collection_name = cleanup_collection
+    collections_before = len(qdrant_client.get_collections().collections)
     response = client.post("/collections", json={"name": collection_name, "vector_size": 1025})
+    collections_after = len(qdrant_client.get_collections().collections)
+
     assert response.status_code == 422
+    assert collections_before == collections_after
