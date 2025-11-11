e72924d3352ceb547fc976058ca0d44d8df3a5f3
mcieslik <01187214@pw.edu.pl>
2025-11-11 17:09:20 +0100

    Fixed tests issues

--- vector_database/tests/test_add_item_api.py
+++ vector_database/tests/test_add_item_api.py
@@ -1,10 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
 from qdrant_client.models import Distance, VectorParams
-
 from vector_database.main import app
 from qdrant_client import QdrantClient
-
 from vector_database.tests.conftest import QDRANT_HOST, QDRANT_PORT, QDRANT_API_KEY
 
 client = TestClient(app)
@@ -28,112 +26,58 @@ def create_and_cleanup_collection():
 
 def test_add_data_success(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1024,
-        "metadata": {"text": "Hello world"}
-    }
+    item = {"vector": [0.1] * 1024, "metadata": {"text": "Hello world"}}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 200
     data = response.json()
     assert data["status"] == "ok"
-    assert data["message"] == f"Item with id {item['id']} added to collection {collection_name}."
-    assert collection_info.result.points_count == 1
-
-def test_add_data_failed_duplicate_id(create_and_cleanup_collection):
-    collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1024,
-        "metadata": {"text": "Hello world"}
-    }
-    client.post(f"/collections/{collection_name}/items", json=item)
-    response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
-
-    assert response.status_code == 400
-    data = response.json()
-    assert data["status"] == "bad request"
-    assert data["message"] == f"Item with id {item['id']} already exists in collection {collection_name}."
-    assert collection_info.result.points_count == 0
-
-def test_add_data_failed_invalid_id_type(create_and_cleanup_collection):
-    collection_name = create_and_cleanup_collection
-    item = {
-        "id": "test",
-        "vector": [0.1] * 1024,
-        "metadata": {"text": "Hello world"}
-    }
-    response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
-
-    assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert data["message"] == f"Item added to collection '{collection_name}'."
+    assert point_count == 1
 
 
 def test_add_data_failed_invalid_vector_type(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": 123,
-        "metadata": {"text": "Hello world"}
-    }
+    item = {"vector": 123, "metadata": {"text": "Hello world"}}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert point_count == 0
 
 def test_add_data_failed_invalid_metadata_type(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1024,
-        "metadata": 1
-    }
+    item = {"vector": [0.1] * 1024, "metadata": 1}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert point_count == 0
 
 def test_add_data_failed_vector_too_short(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1023,
-        "metadata": {"text": "Hello world"}
-    }
+    item = {"vector": [0.1] * 1023, "metadata": {"text": "Hello world"}}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert point_count == 0
 
 def test_add_data_failed_vector_too_long(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1025,
-        "metadata": {"text": "Hello world"}
-    }
+    item = {"vector": [0.1] * 1025, "metadata": {"text": "Hello world"}}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert point_count == 0
 
 def test_add_data_failed_empty_metadata(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item = {
-        "id": 1,
-        "vector": [0.1] * 1025,
-        "metadata": {}
-    }
+    item = {"vector": [0.1] * 1025, "metadata": {}}
     response = client.post(f"/collections/{collection_name}/items", json=item)
-    collection_info = qdrant_client.get_collection(collection_name=collection_name)
+    point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
-    assert collection_info.result.points_count == 0
+    assert point_count == 0
