18cf2038ee6c051085df1290dffbb511021a32ea
mcieslik <01187214@pw.edu.pl>
2025-11-11 16:21:38 +0100

    Added new exception

--- vector_database/exceptions.py
+++ vector_database/exceptions.py
@@ -1,3 +1,8 @@
 class CollectionAlreadyExistsError(Exception):
     """Exception raised when a collection already exists"""
     pass
+
+
+class CollectionDoesNotExistError(Exception):
+    """Exception raised when a collection does not exist"""
+    pass
