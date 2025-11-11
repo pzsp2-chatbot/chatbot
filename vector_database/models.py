7aaf629c00484690d16c2b9bcd5d64f0b0d9cac4
mcieslik <01187214@pw.edu.pl>
2025-11-11 15:43:16 +0100

    Fixed models bugs

--- vector_database/models.py
+++ vector_database/models.py
@@ -1,19 +1,21 @@
-from typing import Dict, Any, Annotated, Optional
+from typing import Dict, Any, Annotated, Optional, ClassVar
 from pydantic import BaseModel, Field, field_validator
 
 
 class CreateCollectionRequest(BaseModel):
-    VECTOR_SIZE = 1024
+    VECTOR_SIZE: ClassVar[int] = 1024
 
-    name: str = Field(..., description="Name of the collection")
-    vector_size: int = Field(VECTOR_SIZE, gt=0, description="Size of vectors in the collection")
+    name: str = Field(..., min_length=1, max_length=64, description="Name of the collection")
+    vector_size: int = Field(VECTOR_SIZE, ge=1024, le=1024, description="Size of vectors in the collection")
 
 
 class AddItemRequest(BaseModel):
+    VECTOR_SIZE: ClassVar[int] = 1024
+
     id: int = Field(..., description="Id of an element")
 
-    vector: Annotated[list[float], Field(min_length=1024, max_length=1024,
-            description="Vector containing exactly 1024 float values")]
+    vector: Annotated[list[float], Field(min_length=VECTOR_SIZE, max_length=VECTOR_SIZE,
+            description=f"Vector containing exactly {VECTOR_SIZE} float values")]
 
     metadata: Dict[str, Any] = Field(..., description="Metadata describing an element")
 
@@ -25,13 +27,14 @@ class AddItemRequest(BaseModel):
 
 
 class SearchItemRequest(BaseModel):
-    RESULTS_BY_DEFAULT = 10
-    FILTER_BY_DEFAULT = None
+    VECTOR_SIZE: ClassVar[int] = 1024
+    RESULTS_BY_DEFAULT: ClassVar[int] = 10
+    FILTER_BY_DEFAULT: ClassVar[str] = None
 
     vector: Annotated[list[float], Field(min_length=1024, max_length=1024,
-            description="Vector containing exactly 1024 float values")]
+            description=f"Vector containing exactly {VECTOR_SIZE} float values")]
 
-    top_k: int = Field(RESULTS_BY_DEFAULT, gt=0, description="Number of results (10 by default)")
+    top_k: int = Field(RESULTS_BY_DEFAULT, gt=0, description=f"Number of results ({RESULTS_BY_DEFAULT} by default)")
 
     filter: Optional[Dict[str, Any]] = Field(FILTER_BY_DEFAULT,
-        description="Optional metadata filter (date of publication, author), None by default")
+        description=f"Optional metadata filter (date of publication, author), {FILTER_BY_DEFAULT} by default")
