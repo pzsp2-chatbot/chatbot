fef657db3591adab38a70b5ea3ce7a7b979087f2
mcieslik <01187214@pw.edu.pl>
2025-11-11 20:45:02 +0100

    Added qdrant id and document id generation

--- vector_database/services/ItemService.py
+++ vector_database/services/ItemService.py
@@ -1,3 +1,4 @@
+import uuid
 from qdrant_client import QdrantClient
 from vector_database.exceptions import CollectionDoesNotExistError
 from vector_database.models import AddItemRequest
@@ -13,7 +14,13 @@ class ItemService:
         except Exception:
             raise CollectionDoesNotExistError(f"Collection '{name}' not found.")
 
-        self.client.upload_collection(collection_name=name, vectors=[request.vector], payload=[request.metadata])
+        qdrant_id = str(uuid.uuid4())
+        document_id = str(uuid.uuid4())
+
+        payload_with_id = dict(request.payload)
+        payload_with_id["document_id"] = document_id
+        self.client.upsert(collection_name=name, points=[{"id": qdrant_id, "vector": request.vector,
+            "payload": payload_with_id}])
 
         return f"Item added to collection '{name}'."
 
