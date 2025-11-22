ee0b505dbfa3b7a44dc4e974cd60cfc43a7093b7
mcieslik <01187214@pw.edu.pl>
2025-11-22 19:45:54 +0100

    Added points formatting to remove id and convert date to string

--- vector_database/services/SearchService.py
+++ vector_database/services/SearchService.py
@@ -3,25 +3,26 @@ from qdrant_client.http.models import ScoredPoint
 from qdrant_client.models import Filter, FieldCondition, MatchValue, Range
 from vector_database.exceptions import CollectionDoesNotExistError
 from vector_database.models import SearchItemRequest
+from vector_database.models_responses import PayloadResponse, ScoredPointResponse
 from vector_database.services.ItemService import ItemService
+from datetime import datetime
 
 
 class SearchService:
     def __init__(self, client: QdrantClient):
         self.client = client
 
-    def search(self, name: str, request: SearchItemRequest) -> list[ScoredPoint]:
+    def search(self, name: str, request: SearchItemRequest) -> list[ScoredPointResponse]:
         try:
             self.client.get_collection(collection_name=name)
         except Exception:
             raise CollectionDoesNotExistError(f"Collection '{name}' not found.")
 
         qdrant_filter = SearchService.create_filter(request)
-
         response = self.client.query_points(collection_name=name, query=request.vector, query_filter=qdrant_filter,
             limit=request.top_k)
 
-        return response.points
+        return SearchService.format_points(response.points)
 
     @staticmethod
     def create_filter(request: SearchItemRequest) -> Filter:
@@ -47,3 +48,19 @@ class SearchService:
             qdrant_filter = None
 
         return qdrant_filter
+
+    @staticmethod
+    def format_points(points: list[ScoredPoint]) -> list[ScoredPointResponse]:
+        formatted_points = []
+
+        for point in points:
+            point_info = point.model_dump()
+
+            payload = point_info["payload"]
+            date_obj = datetime.strptime(str(payload["published_at"]), "%Y%m%d")
+            payload["published_at"] = date_obj.strftime("%Y-%m-%d")
+            formatted_payload = PayloadResponse(**payload)
+
+            formatted_points.append(ScoredPointResponse(score=point_info["score"], payload=formatted_payload))
+
+        return formatted_points
