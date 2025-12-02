ad31254e065cd93efe21eecc3fcfd1ba3d800422
mcieslik <01187214@pw.edu.pl>
2025-12-02 19:01:11 +0100

    Fixed tests issues

--- vector_database/tests/test_add_item_api.py
+++ vector_database/tests/test_add_item_api.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 from fastapi.testclient import TestClient
 from qdrant_client.models import Distance, VectorParams
@@ -41,7 +43,7 @@ def create_and_cleanup_collection():
                     "authors": ["Robert Smith", "Will Smith"],
                     "author_affiliations": ["WUT", "WU"],
                     "abstract": "Sample test1",
-                    "keywords": "IFS, Discrete Space"
+                    "keywords": ["IFS", "Discrete Space"]
             }
         }]
     )
@@ -54,7 +56,7 @@ def create_and_cleanup_collection():
 
 
 item = {
-        "id": 1,
+        "id": 2,
         "vector": [0.1, 0.1, 0.1, 0.1],
         "payload":
         {
@@ -67,14 +69,15 @@ item = {
                 "authors": ["Robert Smith", "Will Smith"],
                 "author_affiliations": ["WUT", "WU"],
                 "abstract": "Sample test2",
-                "keywords": "IFS, Discrete Space"
+                "keywords": ["IFS", "Discrete Space"]
         }
 }
 
 
 def test_add_data_success(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 200
@@ -85,9 +88,10 @@ def test_add_data_success(create_and_cleanup_collection):
 
 
 def test_add_data_failed_invalid_vector_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["vector"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["vector"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -95,36 +99,39 @@ def test_add_data_failed_invalid_vector_type(create_and_cleanup_collection):
 
 
 def test_add_data_failed_invalid_metadata_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"] = 1
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"] = 1
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
     assert point_count == 1
 
 def test_add_data_failed_vector_too_short(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["vector"] = []
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["vector"] = []
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
     assert point_count == 1
 
 def test_add_data_failed_vector_too_long(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["vector"] = [0.1] * 5
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["vector"] = [0.1] * 1025
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
-
     assert response.status_code == 422
     assert point_count == 1
 
 def test_add_data_failed_empty_metadata(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item["payload"] = {}
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy = copy.deepcopy(item)
+    item_copy["payload"] = {}
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -132,19 +139,10 @@ def test_add_data_failed_empty_metadata(create_and_cleanup_collection):
 
 
 def test_add_data_failed_invalid_date_format(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["modified"] = "2025-10-01"
-    response = client.post(f"/collections/{collection_name}/items", json=item)
-    point_count = qdrant_client.count(collection_name=collection_name).count
-
-    assert response.status_code == 422
-    assert point_count == 1
-
-
-def test_add_data_failed_invalid_no_document_id(create_and_cleanup_collection):
-    collection_name = create_and_cleanup_collection
-    item["payload"]["document_id"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["modified"] = "01-01-2025"
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -153,98 +151,108 @@ def test_add_data_failed_invalid_no_document_id(create_and_cleanup_collection):
 
 def test_add_data_failed_invalid_no_title(create_and_cleanup_collection):
     collection_name = create_and_cleanup_collection
-    item["payload"]["title"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy = copy.deepcopy(item)
+    item_copy["payload"]["title"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_created(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["created"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["created"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_modified(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["modified"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["modified"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_language(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["language"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["language"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_doi(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["doi"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["doi"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_url(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["url"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["url"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_authors(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["authors"] = []
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["authors"] = []
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_author_affiliations(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["author_affiliations"] = []
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["author_affiliations"] = []
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_no_abstract(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["abstract"] = ""
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["abstract"] = ""
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
 
 
 def test_add_data_failed_invalid_wrong_title_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["title"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["title"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -252,9 +260,10 @@ def test_add_data_failed_invalid_wrong_title_type(create_and_cleanup_collection)
 
 
 def test_add_data_failed_invalid_wrong_created_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["created"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["created"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -262,9 +271,10 @@ def test_add_data_failed_invalid_wrong_created_type(create_and_cleanup_collectio
 
 
 def test_add_data_failed_invalid_wrong_modified_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["modified"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["modified"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -272,9 +282,10 @@ def test_add_data_failed_invalid_wrong_modified_type(create_and_cleanup_collecti
 
 
 def test_add_data_failed_invalid_wrong_language_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["language"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["language"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -282,9 +293,10 @@ def test_add_data_failed_invalid_wrong_language_type(create_and_cleanup_collecti
 
 
 def test_add_data_failed_invalid_wrong_doi_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["doi"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["doi"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -292,9 +304,10 @@ def test_add_data_failed_invalid_wrong_doi_type(create_and_cleanup_collection):
 
 
 def test_add_data_failed_invalid_wrong_url_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["url"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["url"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -302,9 +315,10 @@ def test_add_data_failed_invalid_wrong_url_type(create_and_cleanup_collection):
 
 
 def test_add_data_failed_invalid_wrong_authors_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["authors"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["authors"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -312,9 +326,10 @@ def test_add_data_failed_invalid_wrong_authors_type(create_and_cleanup_collectio
 
 
 def test_add_data_failed_invalid_wrong_affiliation_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["author_affiliations"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["author_affiliations"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -322,9 +337,10 @@ def test_add_data_failed_invalid_wrong_affiliation_type(create_and_cleanup_colle
 
 
 def test_add_data_failed_invalid_wrong_abstract_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["abstract"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["abstract"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -332,19 +348,10 @@ def test_add_data_failed_invalid_wrong_abstract_type(create_and_cleanup_collecti
 
 
 def test_add_data_failed_invalid_wrong_keywords_type(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["keywords"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
-    point_count = qdrant_client.count(collection_name=collection_name).count
-
-    assert response.status_code == 422
-    assert point_count == 1
-
-
-def test_add_data_failed_invalid_document_id_type(create_and_cleanup_collection):
-    collection_name = create_and_cleanup_collection
-    item["payload"]["document_id"] = 123
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["keywords"] = 123
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 422
@@ -352,10 +359,11 @@ def test_add_data_failed_invalid_document_id_type(create_and_cleanup_collection)
 
 
 def test_add_data_failed_creation_after_modification(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["created"] = "2024-02-20"
-    item["payload"]["modified"] = "2024-02-19"
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["created"] = "2024-02-20"
+    item_copy["payload"]["modified"] = "2024-02-19"
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
     assert response.status_code == 400
@@ -363,11 +371,12 @@ def test_add_data_failed_creation_after_modification(create_and_cleanup_collecti
 
 
 def test_add_data_failed_authors_and_affiliation_lengths_mismatch(create_and_cleanup_collection):
+    item_copy = copy.deepcopy(item)
     collection_name = create_and_cleanup_collection
-    item["payload"]["authors"] = ["author1", "author2"]
-    item["payload"]["authors_affiliation"] = ["WUT"]
-    response = client.post(f"/collections/{collection_name}/items", json=item)
+    item_copy["payload"]["authors"] = ["author1", "author2"]
+    item_copy["payload"]["author_affiliations"] = ["WUT"]
+    response = client.post(f"/collections/{collection_name}/items", json=item_copy)
     point_count = qdrant_client.count(collection_name=collection_name).count
 
-    assert response.status_code == 422
+    assert response.status_code == 400
     assert point_count == 1
